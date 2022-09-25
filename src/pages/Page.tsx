9a7495c0333913e29a8646c566ef0030b43b860f
Jefferson Matheus <jefferson227@gmail.com>
2022-09-25 17:43:29 -0300

    Add calendar in the menu

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,18 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 
 const Page: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
+  const { name } = useParams<{ name: string }>();
 
   return (
     <IonPage>
@@ -24,7 +31,7 @@ const Page: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
+        {name === 'calendar' ? <p>test</p> : <ExploreContainer name={name} />}
       </IonContent>
     </IonPage>
   );
