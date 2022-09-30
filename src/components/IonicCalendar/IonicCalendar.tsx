a74cafdeca4c79a3f6239901668d3d278064365a
Jefferson Matheus <jefferson227@gmail.com>
2022-09-30 07:21:32 -0300

    Style the IonicCalendar

--- src/components/IonicCalendar/IonicCalendar.tsx
+++ src/components/IonicCalendar/IonicCalendar.tsx
@@ -22,59 +22,57 @@ const IonicCalendar = () => {
         </div>
 
         <div className="calendar-grid">
-          <div className="row row-1">
-            <div className="day day-1"></div>
-            <div className="day day-2"></div>
-            <div className="day day-3"></div>
-            <div className="day day-4"></div>
-            <div className="day day-5">1</div>
-            <div className="day day-6">2</div>
-            <div className="day day-7">3</div>
+          <div className="row">
+            <div className="day"></div>
+            <div className="day"></div>
+            <div className="day"></div>
+            <div className="day"></div>
+            <div className="day">1</div>
+            <div className="day">2</div>
+            <div className="day">3</div>
           </div>
 
-          <div className="row row-2">
-            <div className="day day-1">4</div>
-            <div className="day day-2">5</div>
-            <div className="day day-3">6</div>
-            <div className="day day-4">7</div>
-            <div className="day day-5">8</div>
-            <div className="day day-6">9</div>
-            <div className="day day-7">10</div>
+          <div className="row">
+            <div className="day">4</div>
+            <div className="day">5</div>
+            <div className="day">6</div>
+            <div className="day">7</div>
+            <div className="day">8</div>
+            <div className="day">9</div>
+            <div className="day">10</div>
           </div>
 
-          <div className="row row-3">
-            <div className="day day-1">11</div>
-            <div className="day day-2">12</div>
-            <div className="day day-3">13</div>
-            <div className="day day-4">14</div>
-            <div className="day day-5">15</div>
-            <div className="day day-6">16</div>
-            <div className="day day-7">17</div>
+          <div className="row">
+            <div className="day">11</div>
+            <div className="day">12</div>
+            <div className="day">13</div>
+            <div className="day">14</div>
+            <div className="day">15</div>
+            <div className="day">16</div>
+            <div className="day">17</div>
           </div>
 
-          <div className="row row-4">
-            <div className="day day-1">18</div>
-            <div className="day day-2">19</div>
-            <div className="day day-3">20</div>
-            <div className="day day-4">21</div>
-            <div className="day day-5">22</div>
-            <div className="day day-6">23</div>
-            <div className="day day-7">24</div>
+          <div className="row">
+            <div className="day">18</div>
+            <div className="day">19</div>
+            <div className="day">20</div>
+            <div className="day">21</div>
+            <div className="day">22</div>
+            <div className="day">23</div>
+            <div className="day">24</div>
           </div>
 
-          <div className="row row-5">
-            <div className="day day-1">25</div>
-            <div className="day day-2">26</div>
-            <div className="day day-3">27</div>
-            <div className="day day-4">28</div>
-            <div className="day day-5">29</div>
-            <div className="day day-6">30</div>
-            <div className="day day-7"></div>
+          <div className="row">
+            <div className="day">25</div>
+            <div className="day">26</div>
+            <div className="day">27</div>
+            <div className="day">28</div>
+            <div className="day">29</div>
+            <div className="day">30</div>
+            <div className="day"></div>
           </div>
         </div>
       </div>
-
-      <div className="footer">This is the footer</div>
     </section>
   );
 };
