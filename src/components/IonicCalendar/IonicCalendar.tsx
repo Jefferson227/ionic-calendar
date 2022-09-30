310db5fa1898a6dee5978fb498f8d0a5a2014ab1
Jefferson Matheus <jefferson227@gmail.com>
2022-09-30 07:59:28 -0300

    Style the IonicCalendar

--- src/components/IonicCalendar/IonicCalendar.tsx
+++ src/components/IonicCalendar/IonicCalendar.tsx
@@ -35,48 +35,108 @@ const IonicCalendar = () => {
             <div className="day"></div>
             <div className="day"></div>
             <div className="day"></div>
-            <div className="day">1</div>
-            <div className="day">2</div>
-            <div className="day">3</div>
+            <div className="day">
+              <button className="button button-day">1</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">2</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">3</button>
+            </div>
           </div>
 
           <div className="row">
-            <div className="day">4</div>
-            <div className="day">5</div>
-            <div className="day">6</div>
-            <div className="day">7</div>
-            <div className="day">8</div>
-            <div className="day">9</div>
-            <div className="day">10</div>
+            <div className="day">
+              <button className="button button-day">4</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">5</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">6</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">7</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">8</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">9</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">10</button>
+            </div>
           </div>
 
           <div className="row">
-            <div className="day">11</div>
-            <div className="day">12</div>
-            <div className="day">13</div>
-            <div className="day">14</div>
-            <div className="day">15</div>
-            <div className="day">16</div>
-            <div className="day">17</div>
+            <div className="day">
+              <button className="button button-day">11</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">12</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">13</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">14</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">15</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">16</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">17</button>
+            </div>
           </div>
 
           <div className="row">
-            <div className="day">18</div>
-            <div className="day">19</div>
-            <div className="day">20</div>
-            <div className="day">21</div>
-            <div className="day">22</div>
-            <div className="day">23</div>
-            <div className="day">24</div>
+            <div className="day">
+              <button className="button button-day">18</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">19</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">20</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">21</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">22</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">23</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">24</button>
+            </div>
           </div>
 
           <div className="row">
-            <div className="day">25</div>
-            <div className="day">26</div>
-            <div className="day">27</div>
-            <div className="day">28</div>
-            <div className="day">29</div>
-            <div className="day">30</div>
+            <div className="day">
+              <button className="button button-day">25</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">26</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">27</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">28</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">29</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">30</button>
+            </div>
             <div className="day"></div>
           </div>
         </div>
