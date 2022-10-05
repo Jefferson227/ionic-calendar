ff737c2cc54953f9aac87b33ba5a9fcdc8332f00
Jefferson Matheus <jefferson227@gmail.com>
2022-10-05 07:39:36 -0300

    Display calendar days programmatically

--- src/components/IonicCalendar/IonicCalendar.tsx
+++ src/components/IonicCalendar/IonicCalendar.tsx
@@ -120,113 +120,147 @@ const IonicCalendar = () => {
 
         <div className="calendar-grid">
           <div className="row">
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
             <div className="day">
-              <button className="button button-day">1</button>
+              <button className="button button-day">{firstRow[0]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">2</button>
+              <button className="button button-day">{firstRow[1]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">3</button>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="day">
-              <button className="button button-day">4</button>
+              <button className="button button-day">{firstRow[2]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">5</button>
+              <button className="button button-day">{firstRow[3]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">6</button>
+              <button className="button button-day">{firstRow[4]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">7</button>
+              <button className="button button-day">{firstRow[5]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">8</button>
-            </div>
-            <div className="day">
-              <button className="button button-day">9</button>
-            </div>
-            <div className="day">
-              <button className="button button-day">10</button>
+              <button className="button button-day">{firstRow[6]}</button>
             </div>
           </div>
 
           <div className="row">
             <div className="day">
-              <button className="button button-day">11</button>
+              <button className="button button-day">{secondRow[0]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">12</button>
+              <button className="button button-day">{secondRow[1]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">13</button>
+              <button className="button button-day">{secondRow[2]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">14</button>
+              <button className="button button-day">{secondRow[3]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">15</button>
+              <button className="button button-day">{secondRow[4]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">16</button>
+              <button className="button button-day">{secondRow[5]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">17</button>
+              <button className="button button-day">{secondRow[6]}</button>
             </div>
           </div>
 
           <div className="row">
             <div className="day">
-              <button className="button button-day">18</button>
+              <button className="button button-day">{thirdRow[0]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">19</button>
+              <button className="button button-day">{thirdRow[1]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">20</button>
+              <button className="button button-day">{thirdRow[2]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">21</button>
+              <button className="button button-day">{thirdRow[3]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">22</button>
+              <button className="button button-day">{thirdRow[4]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">23</button>
+              <button className="button button-day">{thirdRow[5]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">24</button>
+              <button className="button button-day">{thirdRow[6]}</button>
             </div>
           </div>
 
           <div className="row">
             <div className="day">
-              <button className="button button-day">25</button>
+              <button className="button button-day">{fourthRow[0]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">26</button>
+              <button className="button button-day">{fourthRow[1]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">27</button>
+              <button className="button button-day">{fourthRow[2]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">28</button>
+              <button className="button button-day">{fourthRow[3]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">29</button>
+              <button className="button button-day">{fourthRow[4]}</button>
             </div>
             <div className="day">
-              <button className="button button-day">30</button>
+              <button className="button button-day">{fourthRow[5]}</button>
             </div>
-            <div className="day"></div>
+            <div className="day">
+              <button className="button button-day">{fourthRow[6]}</button>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="day">
+              <button className="button button-day">{fifthRow[0]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{fifthRow[1]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{fifthRow[2]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{fifthRow[3]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{fifthRow[4]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{fifthRow[5]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{fifthRow[6]}</button>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="day">
+              <button className="button button-day">{sixthRow[0]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{sixthRow[1]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{sixthRow[2]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{sixthRow[3]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{sixthRow[4]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{sixthRow[5]}</button>
+            </div>
+            <div className="day">
+              <button className="button button-day">{sixthRow[6]}</button>
+            </div>
           </div>
         </div>
       </div>
